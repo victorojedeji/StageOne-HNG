1f5a26b311df243559f6c694157433d6ccdf5e8a
victorojedeji <ojedejivictordaniel@gmail.com>
2023-09-10 13:40:27 +0100

    stage one task - done

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
-const currDayElement = document.querySelector('[data-testid="currentDayOfTheWeek"]');
-const  timeElement = document.querySelector('[data-testid="currentUTCTime"]');
+const currDayElement = document.querySelector(
+  '[data-testid="currentDayOfTheWeek"]'
+);
+const timeElement = document.querySelector('[data-testid="currentUTCTime"]');
 
+const weekdays = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
 
-const date = new Date();
-const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const updateDay = () => {
+  const date = new Date();
+  const currDayIndex = date.getUTCDay();
+  const currDayName = weekdays[currDayIndex];
+  currDayElement.textContent = currDayName;
+};
 
+const updateTime = () => {
+  setInterval(() => {
+    const date = new Date();
+    const currUTCTime = date.getTime();
+    timeElement.textContent = currUTCTime;
+  }, 100);
+};
 
-const currUTCTime = date.getTime();
-const currDayIndex = date.getDay();
-const currDayName = weekdays[currDayIndex];
-
-
-currDayElement.textContent = currDayName;
-timeElement.textContent = currUTCTime;
-
-
-
+updateDay();
+updateTime();
